c35854558dc41671dfb02a71874a3d62d712badb
Richard Kumahia <Rkumahia@Gmail.com>
2021-02-24 10:43:28 -0500

    Prints a fixed size grid with an "entrance". The grid represents a 2D array of Cell objects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,25 +4,50 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        Cell perfect = new Cell();
-        Cell[] grid = new Cell[10];
-        grid[0].printCell();
+        Cell[][] grid = new Cell[5][11];
+        int start = (int)(Math.random() * grid[0].length);
+        for (int i = 0; i < grid[0].length; i++) {
+            if (i != start) {
+                System.out.print(" _");
+            }
+            else {
+                System.out.print("  ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++ ) {
+                grid[i][j] = new Cell();
+                grid[i][j].printCell();
+            }
+            System.out.println("|");
+        }
+        Stack<Cell> backTrack = new Stack<>();
+
     }
 }
 class Cell {
     String vertWall = "|";
-    String horizWall = "_";
+    String horizWall;
+    boolean[] walls;
+    boolean visited;
 
     // Initializing a boolean array, each boolean represents one side of the cell
     //false = no wall, true = wall
+    //walls[0] = north wall, [1] = south, [2] = east, [3] = west
     Cell() {
-        System.out.println("Construct");
-        boolean[] walls = {false, false, false, false};
+        walls = new boolean[]{true, true, true, true};
         String vertWall = "|";
-        String horizWall = "_";
+        horizWall = "_";
+        visited = false;
     }
 
     public void printCell() {
-        System.out.print(vertWall + horizWall + vertWall);
+        if (this.walls[1]) {
+            System.out.print(vertWall);
+        }
+        if (this.walls[2]) {
+            System.out.print(horizWall);
+        }
     }
 }
