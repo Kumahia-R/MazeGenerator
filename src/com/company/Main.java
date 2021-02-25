7facd4e11daaa425556a22fcfdf50934d1af8cf8
Richard Kumahia <Rkumahia@Gmail.com>
2021-02-24 23:03:20 -0500

    Recursive backtracking generator complete. Will probably add other maze generating algorithms later in the week. Prioritizing maze solver for now.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,21 @@ package com.company;
 import java.util.*;
 import java.io.*;
 public class Main {
-
+    public static Stack<Cell> backTrack = new Stack<>();
     public static void main(String[] args) {
-        int x = 5;
-        int y = 11;
+        int x = 10;
+        int y = 20;
         Grid first = new Grid(x, y);
+        backTrack.push(first.start);
         first.printGrid();
-        recurseBackTrack(first, first.start);
+        recurseBackTrack(first, backTrack.peek());
     }
     public static void recurseBackTrack(Grid recursive, Cell start) {
-        Stack<Cell> backTrack = new Stack<>();
+        /*if (start.horizWall != " ") {
+            start.horizWall = "x";
+        }*/
+        boolean end = false;
+        recursive.printGrid();
         Cell newCell = new Cell();
         boolean choseNewCell = false;
         ArrayList<Integer> directions = new ArrayList<Integer>();
@@ -20,52 +25,84 @@ public class Main {
         directions.add(2);
         directions.add(3);
         Collections.shuffle(directions);
-        backTrack.push(start);
         start.visited = true;
-        while (!choseNewCell) {
+        if (backTrack.size() == 0) {
+            System.out.println("Maze complete");
+            end = true;
+        }
+        while (!choseNewCell && !end) {
+            boolean atBound = false;
+            if (directions.size() == 0) {
+                recurseBackTrack(recursive, backTrack.pop());
+                return;
+            }
             switch (directions.get(0)) {
                 case 0:
-                    newCell = recursive.gridSize[start.rowPos - 1][start.colPos];
-                    if (newCell.visited) {
-                        directions.remove(0);
+                    System.out.println(directions.get(0));
+                    if (start.rowPos > 0) {
+                        newCell = recursive.gridSize[start.rowPos - 1][start.colPos];
+                    } else {
+                        atBound = true;
                     }
-                    else {
+                    if (newCell.visited || atBound) {
+                        directions.remove(0);
+                    } else {
+                        recursive.gridSize[start.rowPos - 1][start.colPos].horizWall = " ";
+                        backTrack.push(newCell);
+                        recurseBackTrack(recursive, newCell);
                         choseNewCell = true;
                     }
                     break;
                 case 1:
-                    newCell = recursive.gridSize[start.rowPos + 1][start.colPos];
-                    if (newCell.visited) {
-                        directions.remove(0);
+                    System.out.println(directions.get(0));
+                    if (start.rowPos < recursive.gridSize.length - 1) {
+                        newCell = recursive.gridSize[start.rowPos + 1][start.colPos];
+                    } else {
+                        atBound = true;
                     }
-                    else {
+                    if (newCell.visited || atBound) {
+                        directions.remove(0);
+                    } else {
+                        start.horizWall = " ";
+                        backTrack.push(newCell);
+                        recurseBackTrack(recursive, newCell);
                         choseNewCell = true;
                     }
                     break;
                 case 2:
-                    newCell = recursive.gridSize[start.rowPos][start.colPos + 1];
-                    if (newCell.visited) {
-                        directions.remove(0);
+                    System.out.println(directions.get(0));
+                    if (start.colPos < recursive.gridSize[0].length - 1) {
+                        newCell = recursive.gridSize[start.rowPos][start.colPos + 1];
+                    } else {
+                        atBound = true;
                     }
-                    else {
+                    if (newCell.visited || atBound) {
+                        directions.remove(0);
+                    } else {
+                        recursive.gridSize[start.rowPos][start.colPos + 1].vertWall = " ";
+                        backTrack.push(newCell);
+                        recurseBackTrack(recursive, newCell);
                         choseNewCell = true;
                     }
                     break;
                 case 3:
-                    newCell = recursive.gridSize[start.rowPos][start.colPos - 1];
-                    if (newCell.visited) {
-                        directions.remove(0);
+                    System.out.println(directions.get(0));
+                    if (start.colPos > 0) {
+                        newCell = recursive.gridSize[start.rowPos][start.colPos - 1];
+                    } else {
+                        atBound = true;
                     }
-                    else {
+                    if (newCell.visited || atBound) {
+                        directions.remove(0);
+                    } else {
+                        start.vertWall = " ";
+                        backTrack.push(newCell);
+                        recurseBackTrack(recursive, newCell);
                         choseNewCell = true;
                     }
                     break;
-                default:
-                    backTrack.pop();
-                    recurseBackTrack(recursive, backTrack.peek());
             }
         }
-        recurseBackTrack(recursive, newCell);
     }
 }
 class Cell {
@@ -80,34 +117,47 @@ class Cell {
     //false = no wall, true = wall
     //walls[0] = north wall, [1] = south, [2] = east, [3] = west
     Cell() {
-        walls = new boolean[]{true, true, true, true};
+        walls = new boolean[]{true, true};
         String vertWall = "|";
         horizWall = "_";
         visited = false;
     }
 
     public void printCell() {
-        if (this.walls[1]) {
+        if (this.walls[0]) {
             System.out.print(vertWall);
         }
-        if (this.walls[2]) {
+        else {
+            System.out.print(" ");
+        }
+        if (this.walls[1]) {
             System.out.print(horizWall);
+        }
+        else {
+            System.out.print(" ");
         }
     }
 }
 class Grid {
     Cell[][] gridSize;
     Cell start;
+    Cell end;
     int startIndex;
+    int endIndex;
 
     Grid(int rows,int cols) {
         gridSize = new Cell[rows][cols];
         start = new Cell();
+        end = new Cell();
         startIndex = (int)(Math.random() * cols);
+        endIndex = (int)(Math.random() * cols);
         for (int i = 0; i < gridSize.length; i++) {
             for (int j = 0; j < gridSize[i].length; j++) {
                 if (i == 0 && j == startIndex) {
                     gridSize[i][j] = start;
+                }
+                else if (i == gridSize.length - 1 && j == endIndex) {
+                    gridSize[i][j] = end;
                 }
                 else {
                     gridSize[i][j] = new Cell();
@@ -118,6 +168,7 @@ class Grid {
         }
     }
     public void printGrid() {
+        end.walls[1] = false;
         for (Cell i : gridSize[0]) {
             if (i != start) {
                 System.out.print(" _");
@@ -127,9 +178,9 @@ class Grid {
             }
         }
         System.out.println();
-        for (int i = 0; i < gridSize.length; i++) {
-            for (int j = 0; j < gridSize[i].length; j++ ) {
-                gridSize[i][j].printCell();
+        for (Cell[] i : gridSize) {
+            for (Cell j : i ) {
+                j.printCell();
             }
             System.out.println("|");
         }
